ea859368073588cd4e9577e3897ad3d1cde0dd2b
13poorvi03 <poorvisharma0306@gmail.com>
2026-01-10 01:00:33 +0530

    second file added

---
+++ Practisecodes/arithmeticoper.java
@@ -0,0 +1,24 @@
+package Practisecodes;
+
+public class arithmeticoper {
+    public static void main(String[] args) {
+        // int x = 5;
+        // int y = 2;
+
+        // System.out.println("Addition: " + (x + y));
+        // System.out.println("Subtraction: " + (x - y));
+        // System.out.println("Multiplication: " + (x * y));
+        // System.out.println("Division: " + (x / y)); // integer division
+   
+   
+   
+        float x = 5;
+        float y = 2;
+
+        System.out.println("Addition: " + (x + y));
+        System.out.println("Subtraction: " + (x - y));
+        System.out.println("Multiplication: " + (x * y));
+        System.out.println("Division: " + (x / y)); // integer division
+    }
+
+}
