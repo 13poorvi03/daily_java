0f63fdf5132f9ba85ccb375576ae378086d17b43
13poorvi03 <poorvisharma0306@gmail.com>
2026-01-10 01:07:56 +0530

    adeddd

---
+++ Practisecodes/sumoftwonum.java
@@ -0,0 +1,14 @@
+package Practisecodes;
+
+public class sumoftwonum {
+    public static void main(String[] args) {
+        
+    
+       int x =15;
+       int y=28;
+       int z =x+y;
+       
+       System.out.println("the total sum of two digiht : " + z);
+ 
+    }
+}
