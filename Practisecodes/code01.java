58151527616ba0c1752ba5692f4ab6c22a747401
13poorvi03 <poorvisharma0306@gmail.com>
2026-01-10 00:44:45 +0530

    added first file

---
+++ Practisecodes/code01.java
@@ -0,0 +1,14 @@
+package Practisecodes;
+
+public class code01 {
+        public static void main(String[] args) {
+        int x;
+        x =500;
+        System.out.println(x);
+        x =600;
+        System.out.println(x);
+        System.out.println(x+1);
+        x=x+5;
+        System.out.println(x);
+        }
+}
