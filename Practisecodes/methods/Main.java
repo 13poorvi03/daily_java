55eff8b989580abbdd8d0403c78ea63783f55267
13poorvi03 <poorvisharma0306@gmail.com>
2026-01-27 17:43:30 +0530

    added

---
+++ Practisecodes/methods/Main.java
@@ -0,0 +1,18 @@
+package Practisecodes.methods;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Math.sqrt(24));
+        System.out.println(Math.sqrt(16));
+        // sqrt is use for finding sq root 
+
+        System.out.println(Math.floor(5.3));
+        System.out.println(Math.floor(14.7));
+        // floor is use for finding round of double number into smallest number 
+
+        System.out.println(Math.ceil(13.6));
+         System.out.println(Math.ceil(18.6));
+         // ceil0 is use for finding round of double number into largest number 
+
+    }
+}
