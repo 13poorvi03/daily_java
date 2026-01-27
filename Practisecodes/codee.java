bec9da3f3400d907bfbdd54f707a5adf65bc5000
13poorvi03 <poorvisharma0306@gmail.com>
2026-01-27 16:08:11 +0530

    - heelo jii

---
+++ Practisecodes/codee.java
@@ -0,0 +1,12 @@
+package Practisecodes;
+
+
+public class codee {
+    static void welcome(){
+        System.out.println("what's up guyzz");
+    }
+    public static void main(String[] args) {
+        welcome();
+    }
+}
+
